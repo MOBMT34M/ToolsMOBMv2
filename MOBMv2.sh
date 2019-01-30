d9fbe9aeaff752c185aab19a584e3b4e566fc492
MOBMT34M <47031766+MOBMT34M@users.noreply.github.com>
2019-01-31 02:29:33 +0700

    Add files via upload

---
+++ MOBMv2.sh
@@ -0,0 +1,553 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+echo ""
+echo $red"L"
+sleep 3
+clear
+echo $green"LO"
+sleep 3
+clear
+echo $yellow"LOA"
+sleep 3
+clear
+echo $blue"LOAD"
+sleep 2
+clear
+echo $green"LOADI"
+sleep 2
+clear
+echo $cyan"LOADIN"
+sleep 2
+clear
+echo $purple"LOADING"
+sleep 2
+clear
+echo $green"LOADING."
+sleep 0.3
+clear
+echo $green"LOADING.."
+sleep 0.3
+clear
+echo $green"LOADING..."
+sleep 0.3
+clear
+echo $green"LOADING...."
+sleep 0.3
+clear
+echo $green"LOADING....."
+sleep 0.3
+clear
+echo $red"Sedang loading , sabar ea mank:)"
+sleep 3
+clear
+echo $yellow"|    |"
+sleep 2
+echo $yellow"|    |"
+sleep 2
+echo $yellow"|    |"
+sleep 2
+echo $yellow"|    |"
+sleep 2
+echo $yellow"|    |"
+sleep 2
+echo $yellow"\    /"
+sleep 2
+echo $yellow" \  /"
+sleep 2
+echo $yellow"LOADING SUCCES........!!!!!!"
+sleep 3
+clear
+toilet -f standard "MOBM" -F gay
+sleep 3
+echo $yellow"WELCOME TO TOOLS INSTALLER MOBM T34M"
+sleep 2
+echo
+echo
+echo ""
+echo $red"[Created by : | 3M0N27 ]"
+sleep 2
+echo $green"[Type : TOOLS INSTALLER MOBM T34M]"
+sleep 2
+echo $yellow"[Team : Man Of Black Mask]"
+sleep 2
+echo $purple"[Channel Youtube : MOBM T34M"
+sleep 2
+echo $red"MAN OF BLACK MASK | OFFICIAL T34M"
+sleep 2
+echo $green"[Thanks to : All admin]"
+sleep 2
+echo $green"[Thanks to : All Members]"
+sleep 2
+echo $purple"[Special Thanks to : Switywabeer | InYour • RISES | H4B1K1_404 | Mr. RKO | DeniiGanzz | Mr. W4HYU"
+sleep 2
+echo $yellow"[Sekutu : Next Cyber Team"
+sleep 2
+echo ""
+echo $green"Silahkan di pilih ea:), pilih 5 gratis 1 :)"
+sleep 2
+echo $yellow"semoga membantu:)"
+sleep 2
+echo $green"[MOBM]====[MOBM]=====[MOBM]====[MOBM]=====[MOBM]=====[MOBM]"
+echo
+echo $red"|   [1] SPAM WA BUKALAPAK"
+echo "[+]=============================================[+]"
+echo $yellow"|   [2] PRANK DI TELFON TOKOPEDIA"
+echo "[+]=============================================[+]"
+echo $green"|   [3] Kalkulator"
+echo "[+]=============================================[+]"
+echo $purple"|   [4] HACK FACEBOOK BRUTE FORCE"
+echo "[+]=============================================[+]"
+echo $cyan"|   [5] HACK INSTAGRAM"
+echo "[+]=============================================[+]"
+echo $red"|   [6] HACK FACEBOOK MBF"
+echo "[+]=============================================[+]"
+echo $green"|   [7] Game Janda"
+echo "[+]=============================================[+]"
+echo $yellow"|   [8] HACK CCTV"
+echo "[+]=============================================[+]"
+echo $purple"|   [9] Menampilkan animasi kereta"
+echo "[+]=============================================[+]"
+echo $cyan"|   [10] Menampilkan informasi system android"
+echo "[+]=============================================[+]"
+echo $red"|   [11] Membuat Script deface"
+echo "[+]=============================================[+]"
+echo $yellow"|   [12] FAKE MIRROR DEFACER"
+echo "[+]=============================================[+]"
+echo $green"|   [13] LITESPAM"
+echo "[+]=============================================[+]"
+echo $blue"|   [14] INSTALL CREWBOT"
+echo "[+]=============================================[+]"
+echo $purple"|   [15] INSTALL METASPLOIT"
+echo "[+]=============================================[+]"
+echo $yellow"|   [16] BLACK HYDRA"
+echo "[+]=============================================[+]"
+echo $green"|   [17] INSTALL Admin Finder"
+echo "[+]=============================================[+]"
+echo $red"|   [18] INSTALL LAZYMUX"
+echo "[+]=============================================[+]"
+echo $cyan"|   [19] INSTALL OSIF"
+echo "[+]=============================================[+]"
+echo $red"|   [20] TOP UP DIAMOND ML ILEGAL"
+echo "[+]=============================================[+]"
+echo $green"   [21] TOP UP DIAMOND FREE FIRE"
+echo "[+]=============================================[+]"
+echo $cyan"|   [22] TOP UP DIAMOND PUBG"
+echo "[+]=============================================[+]"
+echo $red"|   [23] INSTALL TOOLS Pentester"
+echo "[+]=============================================[+]"
+echo $yellow"|   [00] EXIT"
+echo "[+]=============================================[+]"
+echo $green"[Pilih nomornya]"
+read -p "[MOBMT34M]=Nomornya: " no
+
+if [ $no = 1 ] || [ $no = 1 ]
+then
+echo
+toilet -f standard "MOBM" -F gay
+sleep 3
+echo $yellow"spam bukalapak...."
+sleep 2
+apt update && apt upgrade
+pkg install php
+pkg install git
+git clone https://github.com/siputra12/prank
+mv prank$HOME
+cd $HOME/prank
+php wa.php
+fi
+
+if [ $no = 2 ] || [ $no = 2 ]
+then
+echo
+toilet -f standard "MOBM" -F gay
+sleep 3
+echo $red"sedang loading......"
+sleep 1
+apt update && apt upgrade
+pkg install php
+pkg install git
+git clone https://github.com/siputra12/prank
+mv prank $HOME
+cd $HOME/prank
+php call.php
+fi
+
+if [ $no = 3 ] || [ $no = 3 ]
+then
+echo
+toilet -f standard "KALKULATOR" -F gay
+sleep 3
+echo $red"sedang loading......"
+sleep 2
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/3M0N27/kalkulator
+mv kalkulator $HOME
+cd $HOME/kalkulator
+python2 kalkulator.py
+fi
+
+if [ $no = 4 ] || [ $no = 4 ]
+then
+echo
+toilet -f standard "H4B1K1" -F gay
+sleep 3
+echo $yellow"sedang loading...."
+sleep 2
+apt update && apt upgrade
+pkg install python2
+pip2 install mechanize
+pkg install git
+git clone https://github.com/rickyricko302/fb-crackv1
+mv fb-crackv1 $HOME
+cd $HOME/fb-crackv1
+python2 fb-crack.py
+fi
+
+if [ $no = 5 ] || [ $ no = 5 ]
+then
+echo
+toilet -f standard "SweetyWaber" -F gay
+sleep 3
+echo $green"sedang loading......"
+sleep 2
+apt update && apt upgrade
+pkg install python2
+pkg install toilet
+pkg install git
+git clone https://github.com/orangresekcyberteam/HackIG
+mv HackIG $HOME
+cd $HOME/HackIG
+nano pass.txt
+python2 HackIG.py
+fi
+
+if [ $no = 6 ] || [ $no = 6 ]
+then
+echo
+toilet -f standard "Deniiganzz" -F gay
+sleep 3
+echo $green"sedang loading......"
+sleep 2
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Mrdeniganz/MBF
+mv MBF $HOME
+cd $HOME/MBF
+python2 MBF.py
+fi
+
+if [ $no = 7 ] || [ $no = 7 ]
+then
+echo
+toilet -f standard "3M0N27" -F gay
+sleep 3
+echo $red"sedang loading......"
+sleep 2
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/3M0N27/GameJanda
+mv GameJanda $HOME
+cd $HOME/GameJanda
+python2 tugas.py
+fi
+
+if [ $no = 8 ] || [ $no = 8 ]
+then
+echo
+toilet -f standard "CCTV" -F gay
+sleep 3
+echo $blue"sedang loading....."
+sleep 2
+apt update && apt upgrade
+pkg install python2
+pkg install request
+pkg install git
+git clone https://github.com/kancotdiq/ipcs
+mv ipcs $HOME
+cd $HOME/ipcs
+python2 scan.py
+fi
+
+if [ $no = 9 ] || [ $no = 9 ]
+then
+echo
+toilet -f standard "KERETA" -F gay
+sleep 3
+pkg install sl
+sl
+fi
+
+if [ $no = 10 ] || [ $no = 10 ]
+then
+echo
+toilet -f standard "SYSTEM ANDROID" -F gay
+sleep 3
+pkg install neofetch
+neofetch
+fi
+
+if [ $no = 11 ] || [ $no = 11 ]
+then
+echo
+toilet -f standard "Script deface" -F gay
+sleep 3
+echo $yellow"sedang loading....."
+sleep 2
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+chmod +x create.py
+python2 create.py
+fi
+
+if [ $no = 12 ] || [ $no = 12 ]
+then
+echo
+toilet -f standard "FAKE MIRROR" -F gay
+sleep 3
+echo $green"sedang loading....."
+sleep 2
+https://github.com/mr057/master-fake
+mv master-fake $HOME
+cd $HOME/master-fake
+chmod 777 fake.sh
+chmod 777 setup.sh
+sh fake.sh
+fi
+
+if [ $no = 13 ] || [ $no = 13 ]
+then
+echo
+toilet -f standard "MOBM" -F gay
+sleep 2
+echo $red"sedang loading...."
+sleep 2
+pkg install git
+git clone https://github.com/4L13199/LITESPAM
+mv LITESPAM $HOME
+cd $HOME/LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $no = 14 ] || [ $no = 14 ]
+then
+echo
+toilet -f standard "MOBM" -F gay
+sleep 3
+echo $green"sedang loading......."
+sleep 2
+git clone https://github.com/Xeit666h05t/CrewBot
+mv CrewBot $HOME
+cd $HOME/CrewBot
+python2 CrewBot.pyc
+fi
+
+if [ $no = 15 ] || [ $no = 15 ]
+then
+echo
+toilet -f standard "MOBM" -F gay
+sleep 3
+echo $purple"sedang loading......"
+sleep 2
+pkg install autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev \ postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config \ wget make ruby-dev libgrpc-dev termux-tools ncurses ncurses-utils libsodium-dev termux-exec
+git clone https://github.com/rapid7/metasploit-framework
+cd metasploit-framework
+curl -L https://github.com/rapid7/metasploit-framework/archive/4.17.9.tar.gz | tar xz
+cd metasploit-framework-4.17.9
+sed 's|git ls-files|find -type f|' -i metasploit-framework.gemspec
+gem install bundler
+gem install nokogiri -- --use-system-libraries
+gem install bundler -v 1.17.3
+bundle install -j5
+$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+sh msfconsole
+fi
+
+if [ $no = 16 ] || [ $no = 16 ]
+then
+echo
+toilet -f standard "MOBM" -F gay
+sleep 3
+echo $yellow"sedang loading....."
+sleep 2
+pkg install python2
+git clone https://github.com/Gameye98/Black-Hydra
+mv Black-Hydra $HOME
+cd $HOME/Black-Hydra
+python2 blackhydra.py
+fi
+
+if [ $no = 17 ] || [ $no = 17 ]
+then
+echo
+toilet -f standard "MOBM" -F gay
+sleep 3
+echo $green"sedang loading....."
+sleep 2
+pkg install python2
+git clone https://github.com/AdheBolo/AdminFinder
+mv AdminFinder $HOME
+cd $HOME/AdminFinder
+chmod 777 AdminFinder.py
+python2 AdminFinder.py
+fi
+
+if [ $no = 18 ] || [ $no = 18 ]
+then
+echo
+toilet -f standard "MOBM" -F gay
+sleep 3
+echo $green"sedang loading......."
+sleep 2
+pkg install python2
+git clone https://github.com/Gameye98/Lazymux.git
+mv Lazymux.git $HOME
+cd Lazymux
+python2 lazymux.py
+fi
+
+if [ $no = 19 ] || [ $no = 19 ]
+then
+echo
+toilet -f standard "MOBM" -F gay
+sleep 3
+echo $yellow"sedang loading....."
+sleep 2
+apt update && apt upgrade
+apt install python2
+pip2 install requests mechanize
+git clone https://github.com/CiKu370/OSIF.git
+mv OSIF $HOME
+cd $HOME/OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $no = 20 ] || [ $no = 20 ]
+then
+echo
+toilet -f standard "Mobile legend" -F gay
+sleep 3
+echo $yellow"sedang loading....."
+sleep 2
+echo $red"sedang Top Up mank.."
+sleep 4
+echo $green"MOONTON : 1000 DIAMOND will send..."
+sleep 4
+echo $yellow"HahahhahahH... Ciyee yang mencari gratisan:v"
+sleep 3
+echo $green"Modalll mankk:v ... MOONTON buat Gamenya juga modal:v"
+sleep 3
+echo $blue"Sorry mank.. muka lu terlalu buriq"
+sleep 3
+echo $red"jadi gak bisa top up diamond.."
+sleep 3
+echo $yellow"Benerin dulu muka lu entar balik lagi:v"
+sleep 3
+echo $purple"SELAMAT..!! KAMU TERTYPU:v"
+sleep 3
+echo $green"awokwkkwokwok"
+sleep 3
+fi
+
+if [ $no = 21 ] || [ $no = 21 ]
+then
+echo
+toilet -f standard "FREE FIRE" -F gay
+sleep 3
+echo $red"sedang loading....."
+sleep 3
+echo $yellow"Checking diamond........."
+sleep 5
+echo $green"Diamond will send..."
+sleep 4
+echo $yellow"GARENA : 1000 DIAMOND"
+sleep 4
+echo $purple"SELAMAT KAMU TERTYPU Mank:v"
+sleep 4
+echo $red"Kalo mau diamond iya modal mank:v"
+sleep 3
+echo $green"Garena buat gamenya juga modal:v"
+sleep 3
+echo $blue"awokqokqok"
+sleep 3
+fi
+
+if [ $no = 22 ] || [ $no = 22 ]
+then
+echo
+toilet -f standard "PUBG MOBILE" -F gay
+sleep 3
+echo $red"Checking DIAMOND....."
+sleep 5
+echo $green"Masuk ke ilegal....."
+sleep 4
+echo $yellow"Berhasil masuk.."
+sleep 4
+echo $blue"mengambil diamond..."
+sleep 4
+echo $red"hanya mengambil 1000 diamond"
+sleep 4
+echo $blue"diamond akan di kirim.."
+sleep 4
+echo $yellow"SELAMAT... KAMU TERTYPU bgst:v"
+sleep 4
+echo $red"kalo mau diamond ya modal lah kamvank:v"
+sleep 4
+echo $green"Tencent bikin gamenya juga modal :v"
+sleep 4
+echo $purple"lah lu yang mukanya buriq malah kagak modal asw:v"
+sleep 4
+echo $red"awokwokwokwok"
+sleep 3
+fi
+
+if [ $no = 23 ] || [ $no = 23 ]
+then
+echo
+toilet -f standard "H4B1K1" -F gay
+sleep 3
+echo $yellow"sedang loading......"
+sleep 3
+pkg install git
+pkg install python2
+git clone https://github.com/rickyricko302/Pentester
+cd Pentester
+python2 Pentester.py
+fi
+
+if [ $no = 00 ] || [ $no = 00 ]
+then
+echo
+toilet -f standard "THANK YOU" -F gay
+sleep 3
+echo $red"Terima kasih..."
+sleep 3
+echo $green"We are MOBM T34M"
+sleep 3
+echo $yellow"We are legion"
+sleep 3
+echo $blue"expect us"
+sleep 3
+echo $yellow"Exiting........"
+sleep 3
+echo $green"1..."
+sleep 3
+echo $red"2...."
+sleep 3
+echo $yellow"3..."
+sleep 3
+exit
+fi
